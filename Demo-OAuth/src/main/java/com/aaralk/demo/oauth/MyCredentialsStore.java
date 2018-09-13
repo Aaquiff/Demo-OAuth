6ec70824cfa3d669dab27915d7eeeeae8aaae7cd
Aaquiff <aaquiff@gmail.com>
2018-09-13 13:22:11 +0530

    Requsting token works with google OAuth
    code is returned

---
+++ Demo-OAuth/src/main/java/com/aaralk/demo/oauth/MyCredentialsStore.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aaralk.demo.oauth;
+
+/**
+ *
+ * @author aaralk
+ */
+public class MyCredentialsStore {
+    public static String CLIENT_ID = "262313658293-s68omf953q1vc7j2tnsfp52rt3l25s8a.apps.googleusercontent.com";
+    public static String CLIENT_SECRET = "B7_CK9LsZd5lfYJ8L0ezjJbE";
+    
+}
---
+++ Demo-OAuth/src/main/java/com/aaralk/demo/oauth/servlets/OAuth.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aaralk.demo.oauth.servlets;
+
+import com.aaralk.demo.oauth.MyCredentialsStore;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author aaralk
+ */
+@WebServlet(name = "OAuthServlet", urlPatterns = {"/oauth2"})
+public class OAuth extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet OAuthServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet OAuthServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        
+        String redirectUrl = "http://localhost:8080/Demo-OAuth/oauthredirect";
+        String authorizationUrl = String.format("https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=%s&state=12345&redirect_uri=%s&scope=https://www.googleapis.com/auth/drive", MyCredentialsStore.CLIENT_ID, redirectUrl);
+                
+        response.sendRedirect(authorizationUrl);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
