35027151b98794e9435a2f1ff38a7890d8d3d1a8
Aaquiff <aaquiff@gmail.com>
2018-10-05 15:47:44 +0530

    Fetch Google profile information

--- Demo-OAuth/src/main/java/com/aaralk/demo/oauth/servlets/Profile.java
+++ Demo-OAuth/src/main/java/com/aaralk/demo/oauth/servlets/Profile.java
@@ -6,20 +6,17 @@
 package com.aaralk.demo.oauth.servlets;
 
 import com.aaralk.demo.oauth.MyCredentialsStore;
+import com.aaralk.demo.oauth.core.Utility;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.UUID;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,8 +25,34 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author aaralk
  */
-@WebServlet(name = "OAuth2Callback", urlPatterns = {"/oauth2callback"})
-public class OAuth2Callback extends HttpServlet {
+@WebServlet(name = "Profile", urlPatterns = {"/Profile"})
+public class Profile extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Profile</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Profile at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -43,56 +66,38 @@ public class OAuth2Callback extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-                
-        String code = request.getParameter("code");
-        String token = exchangeCodeForToken(code);
-        if(token != null)
-        {
-            String sessionId = UUID.randomUUID().toString();
-            MyCredentialsStore.TokenStore.put(sessionId, token);
+        Cookie sessionCookie = Utility.getSessionToken(request.getCookies());
+        System.out.println("Got session ID: " + sessionCookie.getValue());
+        if(sessionCookie != null) {
+            String token = MyCredentialsStore.TokenStore.get(sessionCookie.getValue());
+            if(token != null) {
+                String res = RequestUserInformation(token);
+                response.getWriter().write(res);
+                response.setStatus(200);
+            }
             
-            response.addHeader("Set-Cookie", "SESSIONID="+ sessionId + ";");
+//response.sendRedirect("/");
+        }
+        else {
+            
         }
         
-        response.sendRedirect("http://localhost:8080/Demo-OAuth");
-        
-
     }
-    
-    private String exchangeCodeForToken(String code) throws RuntimeException{  
-        String redirectUrl = "http://localhost:8080/Demo-OAuth/oauth2callback";
-        //define a variable to store the weather api url and set beijing as it's default value
-        String codeUrl = String.format("https://www.googleapis.com/oauth2/v4/token");
-        StringBuilder strBuf = new StringBuilder();  
+
+    private String RequestUserInformation(String token) {
+        String profileUrl = "https://www.googleapis.com/plus/v1/people/me";
         
+        StringBuilder strBuf = new StringBuilder();  
         HttpURLConnection conn=null;
         BufferedReader reader=null;
-        try{  
-            String urlParameters  = String.format(
-                    "code=%s&" +
-                    "client_id=%s&" +
-                    "client_secret=%s&" +
-                    "redirect_uri=%s&" +
-                    "grant_type=authorization_code",
-                    code, MyCredentialsStore.CLIENT_ID, MyCredentialsStore.CLIENT_SECRET, redirectUrl
-                    );
-            
-            byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
-            int    postDataLength = postData.length;
-            
-            URL url = new URL(codeUrl);  
+        try {  
+            URL url = new URL(profileUrl);  
             conn = (HttpURLConnection)url.openConnection();  
             conn.setDoOutput( true );
             conn.setInstanceFollowRedirects( false );
-            conn.setRequestMethod( "POST" );
+            conn.setRequestMethod( "GET" );
             conn.setRequestProperty("Host", "www.googleapis.com");
-            conn.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded"); 
-            conn.setRequestProperty( "charset", "utf-8");
-            conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
-            conn.setUseCaches( false );
-            try( DataOutputStream wr = new DataOutputStream( conn.getOutputStream())) {
-               wr.write( postData );
-            }
+            conn.setRequestProperty("Authorization", String.format("Bearer %s", token));
             
             conn.connect();
             
@@ -126,12 +131,8 @@ public class OAuth2Callback extends HttpServlet {
             {
                 conn.disconnect();
             }
+            return strBuf.toString();
         }
-
-        String token = strBuf.toString();
-        token = token.substring(token.indexOf("\"access_token\":") + 17);
-        token = token.substring(0, token.indexOf("\""));
-        return token;  
     }
 
     /**
